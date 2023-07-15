fa261d898ace471e4c862fbfbff30706184e8d63
Giuliano Losa <giuliano@losa.fr>
2023-07-15 13:13:10 -0700

    WIP make it faster

--- stellar_network.py
+++ stellar_network.py
@@ -31,6 +31,7 @@ class StellarNetwork:
     A Stellar network is a list of validators, each of which is represented by their public key and has a quorumSet.
 
     :ivar validators: a dictionary mapping public keys to quorumSets
+    :ivar qset: the set of all quorumSets in the network
     """
 
     def __init__(self, validators):
@@ -49,9 +50,14 @@ class StellarNetwork:
                     'innerQuorumSets' : [...]},
                     ...]}}
         """
+        # check that no validator appears twice:
+        if len(validators) != len(set([validator['publicKey'] for validator in validators])):
+            raise ValueError("Duplicate validator")
+        # create a dictionary mapping public keys to QSets:
         self.validators = dict(
             [(validator['publicKey'], QSet.from_json(validator['quorumSet'])) for validator in validators])
         self.sanity_check()
+        self.qsets = set(self.validators.values())
 
     def sanity_check(self):
         """
@@ -79,13 +85,47 @@ class StellarNetwork:
                 check_qset(qset)
             except ValueError as e:
                 raise ValueError("Error in validator {}: {}".format(pk, e))
+            
+    def num_qsets(self):
+        """
+        Return the number of different quorumSets in the network
+        """
+        return len(self.qsets)
 
     def closed_ax(self):
         """
         Return the closedAx formula as computed from the quorumSets of the validators.
-        """        
-        return And(*itertools.chain.from_iterable([closed_ax_validator(validator, qset) for validator,qset in self.validators.items()]))
+        """
+        seen = set()
     
+        def closed_ax_qset(qset):
+            """
+            :param qset: a QSet
+            """
+            if qset in seen:
+                return
+            elems = qset.validators + qset.innerQuorumSets
+            witnesses = list(itertools.combinations(elems, qset.threshold))
+            def witness_to_disj(witness):
+                return Or(*[symbol(e) for e in witness])
+            closed_ax_pos = tvl.Dimp(And(*[witness_to_disj(w) for w in witnesses]), symbol(qset))
+            def witness_to_disj_neg(witness):
+                return Or(*[tvl.Not(symbol(e)) for e in witness])
+            closed_ax_neg = tvl.Dimp(And(*[witness_to_disj_neg(w) for w in witnesses]), tvl.Not(symbol(qset)))
+            seen.add(qset)
+            return [closed_ax_pos, closed_ax_neg] \
+                + list(itertools.chain.from_iterable([closed_ax_qset(innerQset) for innerQset in qset.innerQuorumSets]))  
+
+        def closed_ax_validator(v):
+            assert isinstance(v, str)
+            qset = self.validators[v]
+            # return (qset => v /\ ~qset => ~v):
+            return tvl.And(
+                tvl.Dimp(symbol(qset), symbol(v)),
+                tvl.Dimp(tvl.Not(symbol(qset)), tvl.Not(symbol(v))))
+        
+        return And(*[closed_ax_validator(v) for v in self.validators.keys()])
+
     def network_intertwined(self):
         if len(self.validators) == 1:
             return tvl.Not(tvl.F)
@@ -98,26 +138,13 @@ class StellarNetwork:
     def check_network_intertwined(self):
         return tvl.is_valid(self.network_intertwined())
 
-def hash_qset(qset):
-    """
-    Hash a quorumSet
-    """
-    assert isinstance(qset, dict)
-    # Hash the qset by hashing its json representation. TODO Is that how it's done?
-    return hash(json.dumps(qset, sort_keys=True))
-
 def symbol(x):
     """
-    Associate a symbol with a validator or a quorumSets.
+    Associate a symbol with a validator or a QSet.
     Hopefully, pySMT already does memoization, so we don't need to do it ourselves.
-    For quorumSets, we create symbols using their hash.
     """
-    if isinstance(x, str):
-        return ps.Symbol("V:"+x)
-    elif isinstance(x, dict):
-        return ps.Symbol("Q:"+str(hash(str(x))))
-    else:
-        raise Exception("Wrong type of {} for a symbol: {}".format(x, type(x)))
+    assert isinstance(x, str) or isinstance(x, QSet)
+    return ps.Symbol(str(hash(x)))
 
 def And(*args):
     assert len(args) > 0
@@ -128,44 +155,10 @@ def And(*args):
     
 def Or(*args):
     assert len(args) > 0
-    assert len(args) > 0
     value = args[0]
     for f in args[1:]:
         value = tvl.Or(f, value)
     return value
-
-def closed_ax_elem(e, threshold, validators, innerQuorumSets):
-    """
-    :param e: a public key or an innerQuorumSet
-
-    TODO: we don't need to redo the work if we've already seen the same threshold, validators, and innerQuorumSets combination
-    """
-    elems = validators + innerQuorumSets
-    witnesses = list(itertools.combinations(elems, threshold))
-    def witness_to_disj(witness):
-        return Or(*[symbol(elem) for elem in witness])
-    closed_ax_pos = tvl.Dimp(And(*[witness_to_disj(witness) for witness in witnesses]), symbol(e))
-    def witness_to_disj_neg(witness):
-        return Or(*[tvl.Not(symbol(elem)) for elem in witness])
-    closed_ax_neg = tvl.Dimp(And(*[witness_to_disj_neg(witness) for witness in witnesses]), tvl.Not(symbol(e)))
-    return [closed_ax_pos, closed_ax_neg]
-
-def closed_ax_qset(qset):
-    """
-    Create a variable for the qset and the closedAx formula for the qset.
-    """
-    assert isinstance(qset, dict)
-    return closed_ax_elem(qset, qset['threshold'], qset['validators'], qset['innerQuorumSets']) \
-        + list(itertools.chain.from_iterable([closed_ax_qset(innerQset) for innerQset in qset['innerQuorumSets']]))
-
-def closed_ax_validator(validator, qset):
-    assert isinstance(validator, str)
-    assert isinstance(qset, dict)
-    return closed_ax_elem(validator,
-                            qset['threshold'],
-                            qset['validators'],
-                            qset['innerQuorumSets']) \
-        + list(itertools.chain.from_iterable([closed_ax_qset(innerQset) for innerQset in qset['innerQuorumSets']]))
 
 def intertwined(p, q):
     assert isinstance(p, str) and isinstance(q, str)
